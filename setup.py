0977792e7dd80410149f349595c20971ec69dd70
Steven Myint <git@stevenmyint.com>
2013-06-23 12:01:18 -0700

    Format

--- setup.py
+++ setup.py
@@ -29,12 +29,11 @@ setup(name='vulture',
       py_modules=['wake'],
       scripts=['vulture'],
       classifiers=[
-      'Environment :: Console',
-      'Intended Audience :: Developers',
-      'License :: OSI Approved :: GNU General Public License (GPL)',
-      'Programming Language :: Python',
-      'Programming Language :: Python :: 3'
-      'Topic :: Software Development',
-      'Topic :: Utilities',
-      ],
-      )
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Topic :: Software Development',
+          'Topic :: Utilities',
+      ])
